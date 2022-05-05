084bdde762de35dcd5aabedd15899c66dbb73135
wusize <wusize@sensetime.com>
2022-05-05 19:53:59 +0800

    'fp16'

--- mmdet/models/necks/cbnet_fpn.py
+++ mmdet/models/necks/cbnet_fpn.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 import torch.nn.functional as F
 from mmcv.cnn import xavier_init
+from mmcv.runner import auto_fp16
 
 from ..builder import NECKS
 from .fpn import FPN
@@ -12,6 +13,8 @@ class CBFPN(FPN):
     FPN with weight sharing
     which support mutliple outputs from cbnet
     '''
+
+    @auto_fp16()
     def forward(self, inputs):
         if not isinstance(inputs[0], (list, tuple)):
             inputs = [inputs]
