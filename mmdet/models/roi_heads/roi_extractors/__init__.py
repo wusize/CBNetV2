1152cc4ea749a8d16abd54d21e1b335f30b63b40
wusize <wusize@sensetime.com>
2022-05-05 19:39:48 +0800

    'gc_context'

--- mmdet/models/roi_heads/roi_extractors/__init__.py
+++ mmdet/models/roi_heads/roi_extractors/__init__.py
@@ -1,5 +1,5 @@
 from .base_roi_extractor import BaseRoIExtractor
 from .generic_roi_extractor import GenericRoIExtractor
-from .single_level_roi_extractor import SingleRoIExtractor
+from .single_level_roi_extractor import SingleRoIExtractor, CustomSingleRoIExtractor
 
-__all__ = ['BaseRoIExtractor', 'SingleRoIExtractor', 'GenericRoIExtractor']
+__all__ = ['BaseRoIExtractor', 'SingleRoIExtractor', 'GenericRoIExtractor', 'CustomSingleRoIExtractor']
